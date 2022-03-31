830b0ae798a50eb31150099c56e63215f5b7b9ae
Rich Arce <raarce118@gmail.com>
2022-03-30 22:10:00 -0700

    added more models and routes

--- routes/api/Donor-routes.js
+++ routes/api/Donor-routes.js
@@ -1,5 +1,36 @@
+const Donors = require('../../models/Donors');
+
 const router = require('express').Router();
-//const { Donor, donation, purpose } = require ('../../models'); <--- Need to confirm insertions
+const { Donors } = require ('../../models'); 
 
 //Get all Donors
-router
+router.get('/', (req, res) => {
+     Donors.findAll({
+     attributes: { exclude: ['password'] }
+     })
+         .then(dbDonorsData => res.json(dbDonorsData))
+         .catch(err => {
+             console.log(err);
+             res.status(500).json(err);
+         })
+});
+
+//Get one Donor
+router.get('/:id', (req, res) => {
+     donors.findOne({
+     attributes: { exclude: ['password'] },
+     })
+     })
+         .then(dbDonorsData => {
+         if (!dbDonorsData) {
+         res.status(404).json({ message: 'No user found with this id' });
+         return;
+         }
+         res.json(dbDonorsData);
+         })
+         .catch(err => {
+         console.log(err);
+         res.status(500).json(err);
+         });
+  
+     
