346a0267832a8f19eb6150c89a1c98a5fb2d69fc
Rich Arce <raarce118@gmail.com>
2022-04-03 19:44:35 -0700

    fixed typos and created controllers

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const biofluidsRoutes = require('./biofluids-routes');
+const organsRoutes = require('./organ-routes');
+const purposeRoutes = require('./purpose-Routes');
+const usersRoutes = require('./users-routes');
+
+router.use('/biofluids', biofluidsRoutes);
+router.use('/organs', organsRoutes);
+router.use('/purpose', purposeRoutes);
+router.use('/user', usersRoutes);
+
+module.export = router;
--- models/index.js
+++ models/index.js
@@ -3,4 +3,28 @@ const users = require('./users');
 const organs = require('./organs');
 const purpose = require('./purpose');
 
+// Organs belongsTo Users
+organs.belongsTo(users, {
+    foreignKey: 'donor_id',
+  });
+  
+  // Categories have many Products
+  users.hasMany(organs, {
+    foreignKey: 'donor_id',
+  })
+  
+ // Organs belongsTo Users
+biofluids.belongsTo(users, {
+    foreignKey: 'donor_id',
+  });
+  
+  
+  purpose.belongsToMany(users, {
+      through: {
+        model: users,
+       foreignKey: 'donor_id'
+      }
+  }) 
+  
+
 module.exports = { users, organs, biofluids, purpose };
